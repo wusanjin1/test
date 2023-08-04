164848979b2dd38dd7a9641b73ed674d948aaad6
wusanjin <2150945769@qq.com>
2023-08-04 13:55:43 +0800

    最后提交

--- txbank/src/main/java/com/yc/configs/DataSourceConfig.java
+++ txbank/src/main/java/com/yc/configs/DataSourceConfig.java
@@ -7,10 +7,7 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.TransactionManager;
@@ -76,6 +73,7 @@ public class DataSourceConfig  {
     }
 
     @Bean
+    @Primary
     public DataSource dbcpDataSource(){
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(driverclass);
